47daa521df32e755ec62880985352dd6f6ec3d17
Salomon Aengenheyster-Aber <github@szaa.info>
2026-09-01 18:11:14 +0100

    Package VS Code extension declaratively

--- flake.nix
+++ flake.nix
@@ -7,15 +7,47 @@
   };
 
   outputs =
-    { nixpkgs, remder, ... }:
+    { self, nixpkgs, remder, ... }:
     let
       supportedSystems = [
         "x86_64-linux"
         "aarch64-linux"
       ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      version = (builtins.fromJSON (builtins.readFile ./package.json)).version;
     in
     {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          vsix = pkgs.buildNpmPackage {
+            pname = "vscode-remarkable-vsix";
+            inherit version;
+            src = self;
+            npmDepsHash = "sha256-OjoX+6fT109laSKuZ9kBGRef48fhTmxqhVxL7UTZHpU=";
+            nativeBuildInputs = [ pkgs.vsce ];
+            buildPhase = ''
+              runHook preBuild
+              echo y | vsce package --no-dependencies --allow-missing-repository
+              runHook postBuild
+            '';
+            installPhase = ''
+              install -Dm444 vscode-remarkable-${version}.vsix "$out/vscode-remarkable-${version}.vsix"
+            '';
+          };
+        in
+        {
+          default = pkgs.vscode-utils.buildVscodeExtension {
+            pname = "vscode-remarkable";
+            inherit version;
+            src = "${vsix}/vscode-remarkable-${version}.vsix";
+            vscodeExtPublisher = "undefined";
+            vscodeExtName = "vscode-remarkable";
+            vscodeExtUniqueId = "undefined.vscode-remarkable";
+          };
+        }
+      );
       devShells = forAllSystems (
         system:
         let
